11433fb38a27a33d24369d1f58ecbab0763f3182
y3nk0 <rob.cs.aueb@gmail.com>
2024-05-12 23:33:47 +0300

    added vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./src/**/*.{njk,html,md,js}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
