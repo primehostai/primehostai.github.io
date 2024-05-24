b4704d82fedca0af8e41ced53b2e1579d5889be1
y3nk0 <rob.cs.aueb@gmail.com>
2024-05-24 12:48:15 +0300

    added whatsapp

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,17 +292,4 @@
    */
   new PureCounter();
 
-  // Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".carousel-control-prev").click(function(){
-  $("#myCarousel").carousel("prev");
-});
-
 })();
