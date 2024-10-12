371d021a7e551b3618e3f2c1b10afc6b778782df
y3nk0 <rob.cs.aueb@gmail.com>
2024-10-12 19:38:33 +0300

    header scrolled to hide 100->30

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,7 +82,7 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 40) {
         selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
